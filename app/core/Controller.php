23b226e1d65e262134c068bd4a3a38086b3f1e3d
Artem <polikarpov_aj@groupbwt.com>
2017-04-02 13:25:49 +0300

    [index] Add config call

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -9,7 +9,25 @@ declare(strict_types=1);
  */
 class Controller
 {
-    public function view($view, $data = [])
+    /**
+     * Config handler.
+     * 
+     * @param $config
+     * 
+     * @return array
+     */
+    protected function config($config): array
+    {
+        return require_once '../app/config/' . $config . '.php';
+    }
+
+    /**
+     * View handler.
+     *
+     * @param $view
+     * @param array $data
+     */
+    protected function view($view, $data = [])
     {
         require_once '../app/views/' . $view . '.php';
     }
