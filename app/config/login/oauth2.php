c54bbec51c845fee13f62bad59eb0af685008518
Artem <polikarpov_aj@groupbwt.com>
2017-04-03 00:08:06 +0300

    [CFG] Check config

--- app/config/login/oauth2.php
+++ app/config/login/oauth2.php
@@ -2,7 +2,7 @@
 
 return [
     'auth_params' => [
-        'redirect_uri'  => 'http://oauth.local.com',
+        'redirect_uri'  => 'http://oauth.local.com/auth',
         'response_type' => 'code',
         'client_id'     => '132086215844-7vs9eh9up1ol1abn86i7eoh6m5cr6fih.apps.googleusercontent.com',
         'scope'        => 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
@@ -11,7 +11,7 @@ return [
     'token_params' => [
         'client_id'     => '132086215844-7vs9eh9up1ol1abn86i7eoh6m5cr6fih.apps.googleusercontent.com',
         'client_ecret' => 'kqlYB_pVHIK9E3xlnQ7LsvMO',
-        'redirect_uri'  => 'http://oauth.local.com',
+        'redirect_uri'  => 'http://oauth.local.com/board',
         'grant_type' => 'authorization_code',
     ],
 
