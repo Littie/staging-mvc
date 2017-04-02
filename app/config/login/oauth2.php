6047e127a9ce09588239782354e82f6976fbe87e
Artem <polikarpov_aj@groupbwt.com>
2017-04-02 13:25:18 +0300

    [config] Add config handler

---
+++ app/config/login/oauth2.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'authUri'      => 'https://accounts.google.com/o/oauth2/auth',
+    'tokenUri'     => 'https://www.googleapis.com/oauth2/v4/token',
+    'clientId'     => '132086215844-7vs9eh9up1ol1abn86i7eoh6m5cr6fih.apps.googleusercontent.com',
+    'clientSecret' => 'kqlYB_pVHIK9E3xlnQ7LsvMO',
+    'redirectUri'  => 'http://oauth.local.com',
+];
