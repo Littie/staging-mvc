8bd03c0554efa3e869d3a96b56bc1c0d7ddcb688
Artem <polikarpov_aj@groupbwt.com>
2017-04-02 13:11:53 +0300

    [view] Add view handler method

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -5,6 +5,6 @@ declare(strict_types = 1);
 class LoginController extends Controller
 {
     public function index() {
-        echo 'Login controller';
+        $this->view('login/index');
     }
 }
