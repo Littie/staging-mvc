5d0e5029ad4172180d488d32a0ffc496e3d7e251
Artem <polikarpov_aj@groupbwt.com>
2017-04-02 13:26:31 +0300

    [index] Add config call

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -5,6 +5,10 @@ declare(strict_types = 1);
 class LoginController extends Controller
 {
     public function index() {
+        $config = $this->config('login/oauth2');
+
+        var_dump($config);
+
         $this->view('login/index');
     }
 }
