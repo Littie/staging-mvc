b2f8f065a0a3c487f126ceaafa09652437bf1bad
Artem <polikarpov_aj@groupbwt.com>
2017-04-03 00:07:23 +0300

    [AuthController] Add class

---
+++ app/controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Class AuthController.
+ */
+class AuthController
+{
+    public function index()
+    {
+        echo 'From here should be redirect to board';
+    }
+}
