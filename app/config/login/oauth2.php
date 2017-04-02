f8fdd73e453c5d768d935fca08e08af230d690a7
Artem <polikarpov_aj@groupbwt.com>
2017-04-02 14:04:04 +0300

    [CFG] Change config

--- app/config/login/oauth2.php
+++ app/config/login/oauth2.php
@@ -1,9 +1,20 @@
 <?php
 
 return [
+    'auth_params' => [
+        'redirect_uri'  => 'http://oauth.local.com',
+        'response_type' => 'code',
+        'client_id'     => '132086215844-7vs9eh9up1ol1abn86i7eoh6m5cr6fih.apps.googleusercontent.com',
+        'scope'        => 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+    ],
+
+    'token_params' => [
+        'client_id'     => '132086215844-7vs9eh9up1ol1abn86i7eoh6m5cr6fih.apps.googleusercontent.com',
+        'client_ecret' => 'kqlYB_pVHIK9E3xlnQ7LsvMO',
+        'redirect_uri'  => 'http://oauth.local.com',
+        'grant_type' => 'authorization_code',
+    ],
+
     'authUri'      => 'https://accounts.google.com/o/oauth2/auth',
     'tokenUri'     => 'https://www.googleapis.com/oauth2/v4/token',
-    'clientId'     => '132086215844-7vs9eh9up1ol1abn86i7eoh6m5cr6fih.apps.googleusercontent.com',
-    'clientSecret' => 'kqlYB_pVHIK9E3xlnQ7LsvMO',
-    'redirectUri'  => 'http://oauth.local.com',
 ];
