65bf975a029ada7044d8ab44e657914a79580bcf
Artem <polikarpov_aj@groupbwt.com>
2017-04-04 02:08:48 +0300

    Check config

--- app/config/login/oauth2.php
+++ app/config/login/oauth2.php
@@ -10,11 +10,12 @@ return [
 
     'token_params' => [
         'client_id'     => '132086215844-7vs9eh9up1ol1abn86i7eoh6m5cr6fih.apps.googleusercontent.com',
-        'client_ecret' => 'kqlYB_pVHIK9E3xlnQ7LsvMO',
-        'redirect_uri'  => 'http://oauth.local.com/board',
-        'grant_type' => 'authorization_code',
+        'client_secret' => 'kqlYB_pVHIK9E3xlnQ7LsvMO',
+        'redirect_uri'  => 'http://oauth.local.com/auth',
+        'grant_type'    => 'authorization_code',
     ],
 
     'authUri'      => 'https://accounts.google.com/o/oauth2/auth',
     'tokenUri'     => 'https://www.googleapis.com/oauth2/v4/token',
+    'userUri'      => 'https://www.googleapis.com/oauth2/v1/userinfo', 
 ];
