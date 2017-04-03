52d83cb525c77bc94cd4dd2a8b263be44ff12b56
Artem <polikarpov_aj@groupbwt.com>
2017-04-04 02:07:51 +0300

    Add BoardController

---
+++ app/controllers/BoardController.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Class BoardController
+ */
+class BoardController
+{
+    public function index()
+    {
+        echo 'Return on board page';
+    }
+}
