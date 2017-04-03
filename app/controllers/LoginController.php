461d3dd39361fcacd68df296c649c7e2dc10ff29
Artem <polikarpov_aj@groupbwt.com>
2017-04-04 02:09:32 +0300

    Minor check

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -8,15 +8,11 @@ declare(strict_types = 1);
 class LoginController extends Controller
 {
     public function index() {
-        //'code' => $_GET['code'],
-
         $config = $this->config('login/oauth2');
 
         $authUri = $config['authUri'];
 
         $href = $authUri . '?' . urldecode(http_build_query($config['auth_params']));
-
-//        var_dump($href);
 
         $this->view('login/index', ['href' => $href]);
     }
